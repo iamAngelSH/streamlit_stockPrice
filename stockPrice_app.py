31c49e8726ae1d678495a5212376d1a0277e7222
Angel Santana <91813200+iamAngelSH@users.noreply.github.com>
2022-03-23 14:10:00 -0600

    Update stockPrice_app.py

--- stockPrice_app.py
+++ stockPrice_app.py
@@ -14,6 +14,7 @@ import pandas as pd
 import numpy as np
 from datetime import date
 import datetime
+import sklearn
 from sklearn.model_selection import train_test_split
 from sklearn.svm import SVR
 import streamlit as st
